8206cad50418d20f693dec10ccba76d42413d8e2
PUT YOUR NAME HERE <izzyhasten@gmail.com>
2020-10-01 14:35:37 -0400

    home screen started

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Home from './Components/Home'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Home/>
   </React.StrictMode>,
   document.getElementById('root')
 );
