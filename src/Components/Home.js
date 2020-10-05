de47db8bea060618f0b626fb2b445eae31a9b530
PUT YOUR NAME HERE <izzyhasten@gmail.com>
2020-10-05 09:31:16 -0400

    test run

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,88 +1,8 @@
-import React from "react";
+import React from "react"
 import "../App.scss";
 import shortid from "shortid";
 
 class Home extends React.Component {
-<<<<<<< HEAD
-  state = {
-    activities: [
-      {
-        id: shortid.generate(),
-        activity: "30 minutes of Reading",
-        category: "Educational Activity",
-        reward: 20,
-      },
-      {
-        id: shortid.generate(),
-        activity: "30 minutes of Math",
-        category: "Educational Activity",
-        reward: 20,
-      },
-      {
-        id: shortid.generate(),
-        activity: "Do a puzzle",
-        category: "Educational Activity",
-        reward: 20,
-      },
-      {
-        id: shortid.generate(),
-        activity: "Do 15 Jumping Jacks",
-        category: "physical",
-        reward: 10,
-      },
-      {
-        id: shortid.generate(),
-        activity: "Stretch for 15 minutes",
-        category: "physical",
-        reward: 10,
-      },
-      {
-        id: shortid.generate(),
-        activity: "Spend 1 hour coloring",
-        category: "creative",
-        reward: 15,
-      },
-      {
-        id: shortid.generate(),
-        activity: "Play with Blocks for 1 hour",
-        category: "creative",
-        reward: 20,
-      },
-      {
-        id: shortid.generate(),
-        activity: "Play with Blocks",
-        category: "creative",
-        reward: 15,
-      },
-    ],
-    selectedCategories: [],
-  };
-
-  handleCheckbox = (event) => {
-    let name = event.target.name;
-    let isChecked = event.target.isChecked;
-
-    this.setState((state) => {
-      if (isChecked) {
-      }
-    });
-  };
-
-  render() {
-    return (
-      <>
-        <h1>What types of activities would you like Jake to do today?</h1>
-
-        <p>Creative Activities</p>
-        <input type="checkbox" name="creative" />
-        <p>Physical Acitivities</p>
-        <input type="checkbox" name="physical" />
-        <p>Educational Acitivities</p>
-        <input type="checkbox" name="educational" />
-      </>
-    );
-  }
-=======
     state = {
         activities: [
             {
@@ -170,7 +90,6 @@ class Home extends React.Component {
            </>
         )
     }
->>>>>>> izzys-branch
 }
 
 export default Home;
