159732704b471c310d15f6654bfc901f6d2dce2a
PUT YOUR NAME HERE <izzyhasten@gmail.com>
2020-10-05 08:48:44 -0400

    adding code for handlecheckbox

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -54,20 +54,21 @@ class Home extends React.Component {
                 reward: 15,
             },
         ],
-        selectedCategories:[
-
-        ]
+        selectedCategories:[]
     }
-    handleCheckbox = event =>{
-        let name = event.target.name
-        let isChecked = event.target.isChecked
-
-        this.setState(state=>{
-            if (isChecked){
-
-            }
+    handleCheckbox = event => {
+        let name = event.target.name; //get the name of the category of the box that was checked
+        let isChecked = event.target.checked; //see if we checked the box or unchecked it
+        this.setState(state=>{ //set our state using our current state
+          let newCategories = []
+          if (isChecked){ // if we checked a checkbox, add the name of that category to the array
+            newCategories = [...state.selectedCategories, name]
+          } else {  // if we unchecked a checbox, filter that name out of the array
+            newCategories = state.selectedCategories.filter(category=> category !== name)
+          }
+          return {selectedCategories: newCategories}
         })
-    }
+      }
 
     render() {
         return(
@@ -75,11 +76,17 @@ class Home extends React.Component {
             <h1>What types of activities would you like Jake to do today?</h1>
 
             <p>Creative Activities</p>
-            <input type="checkbox" name="creative"/>
+            <input type="checkbox" name="creative" 
+            defaultChecked={this.state.selectedCategories.includes('creative')}
+            onChange={this.handleCheckbox}/>
             <p>Physical Acitivities</p>
-            <input type="checkbox" name="physical"/>
+            <input type="checkbox" name="physical"
+            defaultChecked={this.state.selectedCategories.includes('physical')}
+            onChange={this.handleCheckbox}/>
             <p>Educational Acitivities</p>
-            <input type="checkbox" name="educational"/>
+            <input type="checkbox" name="educational"
+            defaultChecked={this.state.selectedCategories.includes('educational')}
+            onChange={this.handleCheckbox}/>
            </>
         )
     }
