0835d4a15815d658d824c55220826840bf199432
beckner-uie <petitepaladin@gmail.com>
2020-10-05 20:36:24 -0400

    seperating styles by component

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,16 +1,43 @@
 import React from "react";
-import "./App.scss";
-import "../images_icons_and_more";
 import bxLogOut from "@iconify/icons-bx/bx-log-out"; //This is the log-out icon
 import playCircle from "@iconify/icons-fa-regular/play-circle"; //This is the Home icon
 import { Icon, InlineIcon } from "@iconify/react";
 import styled from "styled-components";
-import globalNav from "./Styles.js";
+import { colors } from "./Styles.js";
+import { ReactComponent as HeaderSvg} from '../assets/Nav/Header.svg';
+import { ReactComponent as FooterSvg} from '../assets/Nav/Footer.svg';
+import {GlobalStyles} from "./Styles.js";
 
 const NavButton = styled.button`
-    color: 
+    color: ${colors.secondary.main}
+    border: none;
+`;
+const NavButtonIcon = styled.svg`
+    fill: ${colors.secondary.main}
+`;
+const NavFooter = styled.footer`
+    position: fixed;
+    bottom: 0;
+`;
+const HeaderStyle = styled.header`
+    width: 100%;
 `;
 
-globalNav(
-    //Put this function in your return, and nest all HTML within this function to style it
-);
+
+export default class Nav extends React.Component {
+    render() {
+        return (
+            <>
+            <GlobalStyles />
+            <HeaderStyle><HeaderSvg /></HeaderStyle>
+            {/* Page Code Goes Here */}
+            <NavFooter>
+            <NavButton>
+                <NavButtonIcon></NavButtonIcon>
+            </NavButton>
+            <FooterSvg />
+            </ NavFooter>
+            </>
+        );
+    }
+}
--- src/Components/Styles.js
+++ src/Components/Styles.js
@@ -4,66 +4,25 @@
 
 import React from 'react';
 import styled from 'styled-components';
-import { ReactComponent as Header} from '../assets/Nav/Header.svg';
-import { ReactComponent as Footer} from '../assets/Nav/Footer.svg';
 import { createGlobalStyle } from 'styled-components';
 
-export default function styles(props) {
-    const colors = {
-        primary: {
-            main: '#1E96FC',
-            dark: '#072AC8',
-            light: '#A2D6F9'
-        },
-        secondary: {
-            main: '#FCF300',
-            dark: '#FFC600'
-        }
+export const colors = {
+    primary: {
+        main: '#1E96FC',
+        dark: '#072AC8',
+        light: '#A2D6F9'
+    },
+    secondary: {
+        main: '#FCF300',
+        dark: '#FFC600'
     }
-    const GlobalStyles = createGlobalStyle`
-    body {
-        margin: 0;
-        padding: 0;
-        height: 100%;
-        position: relative;
-        font-family: "Grandstander", cursive;
-    }
-    `;
-    const Nav = styled.footer`
-        position: fixed;
-        bottom: 0;
-    `;
-    const HeaderStyle = styled.header`
-        width: 100%;
-    `;
-
-    const Headline = styled.h1`
-        text-align: center;
-        width: 90%;
-        margin:  20px auto;
-        color: ${colors.primary.dark};
-    `;
-
-    const NavButton = styled.button`
-    color: ${colors.secondary.main}
-    `;
-    const NavButtonLabel = styled.p;
-    return (
-        <>
-        <GlobalStyles />
-        <HeaderStyle><Header /></HeaderStyle>
-        {props}
-        <Nav>
-        <Footer />
-        </ Nav>
-        </>);
-};
-
-function navButton(props) {
-    return (
-    <>
-    <NavButton>${props.icon}</NavButton>
-    <NavButtonLabel>${props.label}</NavButtonLabel>
-    </>
-    )
-};
+}
+export const GlobalStyles = createGlobalStyle`
+body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    position: relative;
+    font-family: "Grandstander", cursive;
+}
+`;
