8c226579d9b310b31f27320911161f5f6a26e540
January <januaryobrien.ux@gmail.com>
2020-10-06 13:22:02 -0400

    testing splash screen animation not completed

--- src/Components/Splash.js
+++ src/Components/Splash.js
@@ -1,1 +1,25 @@
 import React from 'react';
+import {useSpring} from 'react-spring';
+import logo from '../assets/Splash_Screen/Rejoyce_ Logo.png';
+
+function Splash() {
+  return (
+      <useSpring
+      from={{ opacity: 0, }}
+      to={{ opacity: 1, mrginTop: 3 }}
+      config={{ delay: 1000, duration: 1000 }}
+    >
+      {(props) => (
+        <div>
+          <div>
+          <img src ={logo}/>
+          </div>
+        </div>
+      )}
+    </useSpring>
+  )
+
+}
+
+export default Splash;
+
