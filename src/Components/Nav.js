b0cc8ed28de61f622d70b8fd3e482f439accc3d2
beckner-uie <petitepaladin@gmail.com>
2020-10-05 21:50:28 -0400

    built styled nav buttons, still needs alignment

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import bxLogOut from "@iconify/icons-bx/bx-log-out"; //This is the log-out icon
 import playCircle from "@iconify/icons-fa-regular/play-circle"; //This is the Home icon
+import Add_Activity_Icon from '../assets/Footer_Icons/Add_Activity_Icon.png';
 import { Icon, InlineIcon } from "@iconify/react";
 import styled from "styled-components";
 import { colors } from "./Styles.js";
@@ -9,19 +10,39 @@ import { ReactComponent as FooterSvg} from '../assets/Nav/Footer.svg';
 import {GlobalStyles} from "./Styles.js";
 
 const NavButton = styled.button`
-    color: ${colors.secondary.main}
+    color: ${colors.secondary.main};
     border: none;
+    background: none;
+    display: flex;
+    flex-flow: column nowrap;
+    margin: 10px auto;
+    padding: 0;
+    justify-content: space-around;
+    position: absolute;
+    width: fit-content;
+    height: 70px;
+    font-family: "Grandstander", cursive;
+    text-transform: uppercase;
+    text-align: center;
 `;
-const NavButtonIcon = styled.svg`
-    fill: ${colors.secondary.main}
+const NavButtonIcon = styled.img`
+    margin: auto;
+    width: 60px;
+    height: 60px;
 `;
 const NavFooter = styled.footer`
+    width: 100%;
     position: fixed;
     bottom: 0;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-evenly;
+    align-content: space-evenly;
 `;
 const HeaderStyle = styled.header`
     width: 100%;
 `;
+
 
 
 export default class Nav extends React.Component {
@@ -33,7 +54,20 @@ export default class Nav extends React.Component {
             {/* Page Code Goes Here */}
             <NavFooter>
             <NavButton>
-                <NavButtonIcon></NavButtonIcon>
+                <NavButtonIcon src={Add_Activity_Icon} />
+                Activities
+            </NavButton>
+            <NavButton>
+                <NavButtonIcon src={Add_Activity_Icon} />
+                Activities
+            </NavButton>
+            <NavButton>
+                <NavButtonIcon src={Add_Activity_Icon} />
+                Activities
+            </NavButton>
+            <NavButton>
+                <NavButtonIcon src={Add_Activity_Icon} />
+                Activities
             </NavButton>
             <FooterSvg />
             </ NavFooter>
