8dc8a30446cefad638b4fc7ecfd40b75653a2ab2
beckner-uie <petitepaladin@gmail.com>
2020-10-06 21:08:32 -0400

    added ul and li for styling to Activity Catagories

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "../App.scss";
 import styled from "styled-components";
 import { colors } from "./Styles";
+import { GlobalStyles } from "./Styles";
 import Nav from "./Nav.js";
 import {Link} from "react-router-dom";
 import Splash from './Splash';
@@ -13,17 +14,15 @@ const Headline = styled.h1`
         color: ${colors.primary.dark};
     `;
 const ToggleWrapper = styled.div`
-&:checked,:label,:span {
+    &:checked,:label,:span {
     left: calc(100% - 2px);
     transform: translateX(-100%);
   }
 `;
-const ToggleCheckbox = styled.input`
-&:checkbox {
-height: 0;
-width: 0;
-visibility: hidden;
-}
+const ToggleCheckbox = styled.input.attrs({type: 'checkbox'})`
+  height: 0;
+  width: 0;
+  visibility: hidden;
 `;
 
 const Toggle = styled.label`
@@ -55,38 +54,57 @@ const ToggleButton = styled.span`
     width: 45px;
     }
   `;
+const CategoryList = styled.ul`
+margin: 40px auto;
+list-style: none;
+padding: 0;
+width: 80%;
+`;
+
+const Category = styled.li`
+display: flex;
+margin: 20px 0;
+flex-flow: row;
+justify-content: space-between;
+`;
+  
 
 function Home(props) {
         
     
         return(
         <> 
-
+        <GlobalStyles />
         <Nav />
 
 
-        <Splash>
+        {/* <Splash> */}
             <Headline>What types of activities would you like Jake to do today?</Headline>
+            <CategoryList>
             <ToggleWrapper>
-            <p>Creative Activities</p>
+            <Category><p>Creative Activities</p>
             <ToggleCheckbox type="checkbox" name="creative" 
             defaultChecked={props.selectedCategories.includes('creative')}
             onChange={props.onHandleCheckbox}/>
             <Toggle><ToggleButton/></Toggle>
-            <p>Physical Acitivities</p>
+            </Category>
+            <Category><p>Physical Acitivities</p>
             <ToggleCheckbox type="checkbox" name="physical"
             defaultChecked={props.selectedCategories.includes('physical')}
             onChange={props.onHandleCheckbox}/>
             <Toggle><ToggleButton/></Toggle>
-            <p>Educational Acitivities</p>
+            </Category>
+            <Category><p>Educational Acitivities</p>
             <ToggleCheckbox type="checkbox" name="educational"
             defaultChecked={props.selectedCategories.includes('educational')}
             onChange={props.onHandleCheckbox} />
             <Toggle><ToggleButton/></Toggle>
+            </Category>
             </ToggleWrapper>
+            </CategoryList>
             <Link to="/generator">Generator</Link>
 
-            </Splash>
+            {/* </Splash> */}
            </>
         )
 
