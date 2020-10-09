9aed2e8bd4eecec8166649f75c79a1643497e5e8
Izzy Hasten <izzyhasten@gmail.com>
2020-10-09 08:38:16 -0400

    stash

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -13,29 +13,17 @@ const Headline = styled.h1`
         margin:  20px auto;
         color: ${colors.primary.dark};
     `;
-const ToggleWrapper = styled.div`
-    &:checked,:label,:span {
-    left: calc(100% - 2px);
-    transform: translateX(-100%);
-  }
-`;
-const ToggleCheckbox = styled.input.attrs({type: 'checkbox'})`
-  height: 0;
-  width: 0;
-  visibility: hidden;
-`;
 
-const Toggle = styled.label`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+
+const ToggleContainer = styled.label`
+    display: inline-block;
+    position: relative;
     cursor: pointer;
     width: 50px;
     height: 25px;
-    background: ${colors.secondary.dark};
-    border-radius: 100px;
-    position: relative;
-    transition: background-color .2s;
+    > input {
+      display: none;
+    }
   `;
 
 const ToggleButton = styled.span`
@@ -43,17 +31,30 @@ const ToggleButton = styled.span`
     position: absolute;
     top: 2px;
     left: 2px;
-    width: 20px;
-    height: 20px;
     border-radius: 45px;
     transition: 0.2s;
     background: #fff;
     box-shadow: 0 0 2px 0 rgba(10, 10, 10, 0.29);
 
-    &:active {
-    width: 45px;
+    &:before {
+      position: absolute;
+      width: 20px;
+      height: 20px;
+      background: ${colors.secondary.dark};
+      border-radius: 100px;
+      transition: background-color .2s;
     }
   `;
+
+  const ToggleCheckbox = styled.input`
+&:checked + ${ToggleButton} {
+    background-color: ${colors.secondary.dark};
+    &:before {
+      left: calc(100% - 2px);
+      transform: translateX(-100%);
+    }
+  }
+`;
 const CategoryList = styled.ul`
 margin: 40px auto;
 list-style: none;
@@ -81,26 +82,30 @@ function Home(props) {
         {/* <Splash> */}
             <Headline>What types of activities would you like Jake to do today?</Headline>
             <CategoryList>
-            <ToggleWrapper>
             <Category><p>Creative Activities</p>
-            <input type="checkbox" name="creative" 
+            <ToggleContainer>
+            <ToggleCheckbox type="checkbox" name="creative" 
             defaultChecked={props.selectedCategories.includes('creative')}
             onChange={props.onHandleCheckbox}/>
-            <Toggle><ToggleButton/></Toggle>
+            <ToggleButton/>
+            </ToggleContainer>
             </Category>
             <Category><p>Physical Acitivities</p>
-            <input type="checkbox" name="physical"
+            <ToggleContainer>
+            <ToggleCheckbox type="checkbox" name="physical"
             defaultChecked={props.selectedCategories.includes('physical')}
             onChange={props.onHandleCheckbox}/>
-            <Toggle><ToggleButton/></Toggle>
+            <ToggleButton/>
+            </ToggleContainer>
             </Category>
             <Category><p>Educational Acitivities</p>
-            <input type="checkbox" name="educational"
+            <ToggleContainer>
+            <ToggleCheckbox type="checkbox" name="educational"
             defaultChecked={props.selectedCategories.includes('educational')}
             onChange={props.onHandleCheckbox} />
-            <Toggle><ToggleButton/></Toggle>
+            <ToggleButton/>
+            </ToggleContainer>
             </Category>
-            </ToggleWrapper>
             </CategoryList>
             <Link to="/generator">Generator</Link>
 
